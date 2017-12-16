bf7e412102aa3db20d6bc3fda9308831fba387ea
picojazz <picojazzz@gmail.com>
2017-12-16 16:28:30 +0000

    add listview of offer

--- app/src/main/java/com/example/supschool/picojazzemploiapp/HomeActivity.java
+++ app/src/main/java/com/example/supschool/picojazzemploiapp/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.supschool.picojazzemploiapp;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +14,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private ListView homeListView;
+    private List<Offer> listOffers;
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +46,18 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        homeListView = (ListView) findViewById(R.id.homeListView);
+        listOffers = new ArrayList<>();
+        dialog = new ProgressDialog(this);
+        dialog.setMessage(getString(R.string.waiting));
+
+        //api offers
+        OfferServer offerServer = new OfferServer();
+        offerServer.execute("http://192.168.56.1:8080/api/offers");
+
+        //adapter
+        /*CustomAdaptder customAdaptder = new CustomAdaptder();
+        homeListView.setAdapter(customAdaptder);*/
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -74,7 +109,7 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_moncv) {
-            // Handle the camera action
+
         } else if (id == R.id.nav_favoris) {
 
         }
@@ -82,5 +117,97 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    protected class OfferServer extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                HttpClient client = new DefaultHttpClient();
+                HttpGet get = new HttpGet(params[0]);
+                ResponseHandler<String> buffer = new BasicResponseHandler();
+                String result = client.execute(get, buffer);
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            dialog.dismiss();
+            if (s == null) {
+                Toast.makeText(HomeActivity.this, "erreur de connexion", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            try {
+
+               // JSONObject jsonObject = new JSONObject(s);
+                JSONArray jsonArray = new JSONArray(s);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject element = jsonArray.getJSONObject(i);
+                    /*String dateStr = element.getString("dateCreate");
+                    SimpleDateFormat  sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    Date  date = sdf.parse(dateStr);*/
+                    Offer offer = new Offer(element.getString("title"),element.getString("place"),element.getString("contract"),element.getString("dateCreate"));
+                    listOffers.add(offer);
+                }
+
+                //for (int i = 0 ; i < listOffers.size() ; i++){
+                 //   System.out.println(listOffers.get(i).toString());
+                //}
+                //Toast.makeText(HomeActivity.this, listOffers.get(0).toString(), Toast.LENGTH_SHORT).show();
+                CustomAdaptder customAdaptder = new CustomAdaptder();
+                homeListView.setAdapter(customAdaptder);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+    class CustomAdaptder extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return listOffers.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup parent) {
+            view = getLayoutInflater().inflate(R.layout.offer_layout, null);
+
+            TextView title = (TextView)view.findViewById(R.id.offerTitle);
+            TextView place = (TextView)view.findViewById(R.id.offerPlace);
+            TextView contract = (TextView)view.findViewById(R.id.offerContract);
+            TextView date = (TextView)view.findViewById(R.id.offerDate);
+
+            title.setText(listOffers.get(i).getTitle());
+            place.setText(listOffers.get(i).getPlace());
+            contract.setText(listOffers.get(i).getContract());
+            date.setText(listOffers.get(i).getDateCreate());
+
+            return view;
+        }
     }
 }
---
+++ app/src/main/java/com/example/supschool/picojazzemploiapp/Offer.java
@@ -0,0 +1,139 @@
+package com.example.supschool.picojazzemploiapp;
+
+
+import java.util.Date;
+//import java.util.List;
+
+
+public class Offer {
+
+    private Long id;
+    private String title;
+
+    private String about;
+    private String place;
+    private int lon;
+    private int lat;
+    private String contract;
+    private int Salary;
+    private String dateCreate;
+
+    //private List<Tags> tags;
+
+
+
+    public Offer() {
+    }
+
+    public Offer(String about, String place, int lon, int lat, String contract, int salary, String dateCreate) {
+        this.about = about;
+        this.place = place;
+        this.lon = lon;
+        this.lat = lat;
+        this.contract = contract;
+        Salary = salary;
+        this.dateCreate = dateCreate;
+    }
+
+    public Offer(String title, String place, String contract, String dateCreate) {
+        this.title = title;
+        this.place = place;
+        this.contract = contract;
+        this.dateCreate = dateCreate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getContract() {
+        return contract;
+    }
+
+    public void setContract(String contract) {
+        this.contract = contract;
+    }
+
+    public int getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(int salary) {
+        Salary = salary;
+    }
+
+    public String getDateCreate() {
+        return dateCreate;
+    }
+
+    public void setDateCreate(String dateCreate) {
+        this.dateCreate = dateCreate;
+    }
+
+    public int getLon() {
+        return lon;
+    }
+
+    public void setLon(int lon) {
+        this.lon = lon;
+    }
+
+    public int getLat() {
+        return lat;
+    }
+
+    public void setLat(int lat) {
+        this.lat = lat;
+    }
+
+    /*public List<Tags> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Tags> tags) {
+        this.tags = tags;
+    }*/
+
+    @Override
+    public String toString() {
+        return "Offer{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", about='" + about + '\'' +
+                ", place='" + place + '\'' +
+                ", lon=" + lon +
+                ", lat=" + lat +
+                ", contract='" + contract + '\'' +
+                ", Salary=" + Salary +
+                ", dateCreate=" + dateCreate +
+                '}';
+    }
+}
